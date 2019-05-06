c702b3778e972dba011b20431e45df9ebd7d621c
psywombats <psy_wombats@wombatrpgs.net>
2019-05-05 20:26:45 -0700

    Alpha panels

--- erebus/Assets/Scripts/Map/Map3D/MapCamera3D.cs
+++ erebus/Assets/Scripts/Map/Map3D/MapCamera3D.cs
@@ -11,6 +11,7 @@ public class MapCamera3D : MapCamera {
 
     public void Awake() {
         lastFixedZ = transform.position.z;
+        GetComponent<Camera>().transparencySortMode = TransparencySortMode.Orthographic;
     }
 
     public void LateUpdate() {
